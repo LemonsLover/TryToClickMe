9c18dd48271ff1d7becfabf1e8557aaafde9ea18
Yehor Ovseukov <ki201_oeyu@student.ztu.edu.ua>
2021-09-14 23:25:09 +0300

    Add project files.

---
+++ TryToClickMe/Form1.Designer.cs
@@ -0,0 +1,71 @@
+﻿
+namespace TryToClickMe
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(335, 191);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(100, 50);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Click me!";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            this.button1.MouseEnter += new System.EventHandler(this.button1_MouseEnter);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(784, 461);
+            this.Controls.Add(this.button1);
+            this.MaximizeBox = false;
+            this.MaximumSize = new System.Drawing.Size(800, 500);
+            this.MinimizeBox = false;
+            this.MinimumSize = new System.Drawing.Size(800, 500);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Try to click me !";
+            this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.Form1_FormClosed);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.Click += new System.EventHandler(this.Form1_Click);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ TryToClickMe/Form1.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Diagnostics;
+using System.Threading;
+
+namespace TryToClickMe
+{
+    public partial class Form1 : Form
+    {
+        private int lives { get; set; } = 5;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("kak ti eto sdelal ? Suka !");
+            Application.Exit();
+        }
+
+        private void button1_MouseEnter(object sender, EventArgs e)
+        {
+            var rnd = new Random();
+            var thisButton = sender as Button;
+
+            int newX = rnd.Next(this.Width - thisButton.Width);
+            int newY = rnd.Next(this.Height - thisButton.Height);
+
+            thisButton.Location = new Point(newX, newY);
+            thisButton.BackColor = Color.FromArgb(rnd.Next(255), rnd.Next(255), rnd.Next(255));
+        }
+
+        private void Form1_Click(object sender, EventArgs e)
+        {
+            if (lives > 0)
+                this.Text = "Try to click me! " + $"Lives left: {--lives}";
+            else
+            {
+                MessageBox.Show("You loose !", "Obidna", MessageBoxButtons.OK);
+                Application.Exit();
+            }
+        }
+
+        private async void Form1_Load(object sender, EventArgs e)
+        {
+            this.Text = "Try to click me! " + $"Lives left: {lives}";
+            await Task.Run(() => ButtonSpawner());
+        }
+
+        private void ButtonSpawner()
+        {
+            var rnd = new Random();
+            int a = 10;
+            while (true)
+            {
+                var button = new Button();
+                button.Size = new Size(100, 50);
+
+                int newX = rnd.Next(this.Width - button.Width);
+                int newY = rnd.Next(this.Height - button.Height);
+
+                button.Location = new Point(newX, newY);
+
+                this.Invoke(new MethodInvoker(() => { this.BackColor = Color.FromArgb(rnd.Next(255), rnd.Next(255), rnd.Next(255)); }));
+            }
+
+        }
+
+        private void Form1_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            Application.Exit();
+        }
+    }
+}
